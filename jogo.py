ecac0c94211093b12964a0c69a8d06389105eae1
rxpjunior <rxpjunior@gmail.com>
2021-04-08 20:31:24 -0300

    Protótipo pronto

--- jogo.py
+++ jogo.py
@@ -17,6 +17,38 @@ dicioPerguntas = {
     }
 }
 
+print()
+
+respostasCertas = 0
+
 for perguntaKeys, perguntaValues in dicioPerguntas.items(): # O .items() designa todos os itens do dicionario
-    print(perguntaKeys, ' : ', perguntaValues)              # As chaves e valores, imprimindo tudo
+    print(f'{perguntaKeys}:{perguntaValues["Pergunta"]}')
+
+    print('Respostas: ')
+
+    for respostaKeys, respostaValues in perguntaValues['Respostas'].items():
+        print(f'[{respostaKeys}]: {respostaValues}')
+
+    resposta_usuario = input('Sua resposta: ')
+
+    if resposta_usuario == perguntaValues['Resposta_certa']:
+        print('Ótimo, você acertou, Parabéns!!!!')
+        respostasCertas += 1
+    else:
+        print('Puxa, vc errou. Tem que ir mais na EBD!!!!')
+
     print()
+
+quantidadePerguntas = len(dicioPerguntas)
+porcentagemAcerto = respostasCertas / quantidadePerguntas * 100
+print(f'Você respondeu corretamente {respostasCertas} perguntas.')
+print(f'Sua porcentagem de acerto foi {porcentagemAcerto:.2f}%.')
+
+if porcentagemAcerto == 100:
+    print("Parabéns, você parece ser um obreiro aprovado!!! Continue assim!!!")
+elif porcentagemAcerto >= 75:
+    print("Seu desempenho foi bom. Leia mais a Bíblia para melhorar!")
+elif porcentagemAcerto >= 50:
+    print("Sua situação está meio complicada. Acorde cedo domingo de manhã e vá para EBD!")
+else:
+    print("Misericórdia, se você ler a Bíblia 30 minutos por dia e ir na EBD todos os domingos você melhora!")
